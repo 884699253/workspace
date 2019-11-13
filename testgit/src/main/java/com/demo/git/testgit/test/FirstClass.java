243fab601ca6c39edec9b537294aa1629ed105f5
lzb <lzb@qq.com>
2019-11-13 15:49:20 +0800

    添加本地工程

---
+++ testgit/src/main/java/com/demo/git/testgit/test/FirstClass.java
@@ -0,0 +1,17 @@
+package com.demo.git.testgit.test;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello project1");
+        System.out.println("hello project1");
+        System.out.println("hello project1");
+        System.out.println("hello project1");
+        System.out.println("hello project1");
+        System.out.println("Jlkjlkj");
+    }
+
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
